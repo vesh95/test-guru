f039638bcf1917cc2f235639fa1fc5f99f61eabe
vesh95 <vesh95.17@ya.ru>
2019-12-08 23:44:16 +0500

    Revert to start branch

--- db/migrate/20191208104557_test_answer_default_values.rb
+++
@@ -1,6 +0,0 @@
-class TestAnswerDefaultValues < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default :tests, :level, 1
-    change_column_default :answers, :correct, false
-  end
-end
