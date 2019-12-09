3ea5f4f44e3d9f69b1dce1288352d21a51a81beb
vesh95 <vesh95.17@ya.ru>
2019-12-09 11:36:09 +0500

    add names_by_category method to Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.names_by_category(name)
+    category_id = Category.where(title: name)
+    where(category_id: category_id).order(title: :desc).pluck(:title)
+  end
 end
