5ba1598105453042d46cc81d75226adf46551274
vesh95 <vesh95.17@ya.ru>
2019-12-09 13:56:30 +0500

    add tests_by_level to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
+  def tests_by_level(level)
+    Test.where(level: level).where('users.id = ?', id)
+    .joins("LEFT JOIN user_tests ON tests.id = user_tests.test_id")
+    .joins("LEFT JOIN users ON users.id = user_tests.user_id")
+  end
 end
