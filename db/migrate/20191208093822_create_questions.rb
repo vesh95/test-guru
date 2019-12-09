3912c23a687ae433b2e8e655573a147c6b888724
vesh95 <vesh95.17@ya.ru>
2019-12-09 08:46:24 +0500

    returned references in migrations

--- db/migrate/20191208093822_create_questions.rb
+++ db/migrate/20191208093822_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :body
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20191208094117_create_answers.rb
+++ db/migrate/20191208094117_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.text :body
+      t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
