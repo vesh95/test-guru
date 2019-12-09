2c96732e583e456864c43478099c315ea19ef101
vesh95 <vesh95.17@ya.ru>
2019-12-09 08:48:18 +0500

    recreate database

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,11 @@ ActiveRecord::Schema.define(version: 2019_12_08_192655) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "correct", default: false, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,15 +29,19 @@ ActiveRecord::Schema.define(version: 2019_12_08_192655) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 1
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +51,7 @@ ActiveRecord::Schema.define(version: 2019_12_08_192655) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
