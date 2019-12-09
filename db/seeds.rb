2bd832aaa1e6564569cfd1447e785166be8476ca
vesh95 <vesh95.17@ya.ru>
2019-12-09 10:34:52 +0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+category_backend = Category.create!(title: 'Backend')
+category_frontend = Category.create!(title: 'Frontend')
+ruby_test = Test.create!(title: 'Ruby', level: 1, category_id: category_backend.id)
+ruby_test_question = Question.create(body: 'Что вернёт метод select', test_id: ruby_test.id)
+Answer.create!(body: 'Вервый попавшийся элемент в массиве', question_id: ruby_test_question.id)
+Answer.create!(body: 'Подмассив, соответствующий условию выборки', question_id: ruby_test_question.id)
+Test.create!(title: 'Express', level: 2, category_id: category_backend.id)
+Test.create!(title: 'ReactJS', level: 2, category_id: category_frontend.id)
