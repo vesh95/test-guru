86715a4d894770a99c8b24e6b191da8789c118ab
vesh95 <vesh95.17@ya.ru>
2019-12-11 00:39:24 +0500

    corrected seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,25 @@
+test_owner = User.create!(login: 'TestOwner', password_digest: 'OwnerPassword')
 categories = Category.create!([{ title: 'Backend' }, { title: 'Frontend' }])
 tests = Test.create!([
-  { title: 'Ruby', level: 1, category_id: categories[0].id },
-  { title: 'Express', level: 2, category_id: categories[0].id },
-  { title: 'ReactJS', level: 2, category_id: categories[1].id }
+  { title: 'Ruby', level: 1, category: categories[0], author: test_owner },
+  { title: 'Express', level: 2, category: categories[0], author: test_owner },
+  { title: 'ReactJS', level: 2, category: categories[1], author: test_owner }
 ])
 
 questions = Question.create([
-  { body: 'Что вернёт метод select', test_id: tests[0].id },
+  { body: 'Что вернёт метод select', test: tests[0] },
 ])
 
 Answer.create!([
-  { body: 'Вервый попавшийся элемент в массиве', question_id: questions[0].id },
-  { body: 'Подмассив, соответствующий условию выборки', question_id: questions[0].id }
+  { body: 'Вервый попавшийся элемент в массиве', question: questions[0] },
+  { body: 'Подмассив, соответствующий условию выборки', question: questions[0] }
 ])
 
 user = User.create(login: 'TestUser', password_digest: 'TestPassword')
 UserTest.create([
-  { user_id: user.id, test_id: tests[0].id },
-  { user_id: user.id, test_id: tests[1].id },
-  { user_id: user.id, test_id: tests[2].id }
+  { user: user, test: tests[0] },
+  { user: user, test: tests[1] },
+  { user: user, test: tests[2] }
 ])
 
 Test.create!( title: 'Owned test', category: categories[0], author: user)
