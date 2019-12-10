ad78bfb4b83a4b3ca7ef9537eda502cb8ff13b99
vesh95 <vesh95.17@ya.ru>
2019-12-10 10:11:56 +0500

    refactored tests_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # has_secure_password
   def tests_by_level(level)
-    Test.where(level: level).where('users.id = ?', id)
+    Test
     .joins("LEFT JOIN user_tests ON tests.id = user_tests.test_id")
-    .joins("LEFT JOIN users ON users.id = user_tests.user_id")
+    .where(level: level, user_tests: { user_id: id })
   end
 end
