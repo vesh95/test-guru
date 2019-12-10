892f42ab554b409e3c92de9a61fd4e1eb0dbd2ec
vesh95 <vesh95.17@ya.ru>
2019-12-11 00:14:40 +0500

    change symbols to string in class_name params

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: :User, foreign_key: :author_id, optional: true
+  belongs_to :author, class_name: 'User', foreign_key: :author_id, optional: true
   has_many :user_tests
   has_many :users, through: :user_tests, dependent: :destroy
   has_many :questions, dependent: :destroy
