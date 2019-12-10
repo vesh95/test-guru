204547f3cac444f680c8be03490f4a0ca8b021ae
vesh95 <vesh95.17@ya.ru>
2019-12-10 11:14:41 +0500

    refactored names_by_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.names_by_category(name)
-    category_id = Category.where(title: name)
-    where(category_id: category_id).order(title: :desc).pluck(:title)
+    joins('JOIN categories ON categories.id = tests.category_id')
+    .where(categories: { title: name }).pluck(:title)
   end
 end
