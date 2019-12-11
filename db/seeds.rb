43c05b8f620e862b96cc3032043de7017dead4f7
vesh95 <vesh95.17@ya.ru>
2019-12-11 14:20:20 +0500

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,25 @@ tests = Test.create!([
   { title: 'ReactJS', level: 2, category: categories[1], author: test_owner }
 ])
 
-questions = Question.create([
+questions = Question.create!([
   { body: 'Что вернёт метод select', test: tests[0] },
+  { body: 'Что такое Express?', test: tests[1] },
+  { body: 'Что делает setState?', test: tests[2] },
+  { body: 'Что такое ORM?', test: tests[0] },
+  { body: 'Для чего нужна библиотека Redux?', test: tests[2] }
 ])
 
 Answer.create!([
-  { body: 'Вервый попавшийся элемент в массиве', question: questions[0] },
-  { body: 'Подмассив, соответствующий условию выборки', question: questions[0] }
+  { body: 'Вервый попавшийся элемент в массиве', question: questions[0], correct: true },
+  { body: 'Подмассив, соответствующий условию выборки', question: questions[0] },
+  { body: 'JS бекэнд фреймворк', question: questions[1], correct: true },
+  { body: 'Фронтэнд фреймворк', question: questions[1] },
+  { body: 'Обновляет состояние компонента', question: questions[2], correct: true },
+  { body: 'Возвращает текущее состояние компонента', question: questions[2] },
+  { body: 'Object relation mapping', question: questions[3], correct: true },
+  { body: 'Object rational mapping', question: questions[3] },
+  { body: 'Для изменения dom-объектов', question: questions[4] },
+  { body: 'Для управления состоянием приложения', question: questions[4], correct: true }
 ])
 
 user = User.create(login: 'TestUser', password_digest: 'TestPassword')
