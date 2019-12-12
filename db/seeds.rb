29083fd0a004286bc9d29e969f53fadc9cd78023
vesh95 <vesh95.17@ya.ru>
2019-12-12 15:29:54 +0500

    add question must have 1..4 answers validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,28 +15,30 @@ tests = Test.create!([
 ])
 
 questions = Question.create!([
-  { body: 'Что вернёт метод select', test: tests[0] },
-  { body: 'Что такое Express?', test: tests[1] },
-  { body: 'Что делает setState?', test: tests[2] },
-  { body: 'Что такое ORM?', test: tests[0] },
-  { body: 'Для чего нужна библиотека Redux?', test: tests[2] }
-])
-
-Answer.create!([
-  { body: 'Первый попавшийся элемент в массиве', question: questions[0], correct: true },
-  { body: 'Подмассив, соответствующий условию выборки', question: questions[0] },
-  { body: 'JS бекэнд фреймворк', question: questions[1], correct: true },
-  { body: 'Фронтэнд фреймворк', question: questions[1] },
-  { body: 'Обновляет состояние компонента', question: questions[2], correct: true },
-  { body: 'Возвращает текущее состояние компонента', question: questions[2] },
-  { body: 'Object relation mapping', question: questions[3], correct: true },
-  { body: 'Object rational mapping', question: questions[3] },
-  { body: 'Для изменения dom-объектов', question: questions[4] },
-  { body: 'Для управления состоянием приложения', question: questions[4], correct: true }
+  { body: 'Что вернёт метод select', test: tests[0], answers: [
+    Answer.new(body: 'Первый попавшийся элемент в массиве', correct: true),
+    Answer.new(body: 'Подмассив, соответствующий условию выборки')
+  ]},
+  { body: 'Что такое Express?', test: tests[1], answers: [
+    Answer.new(body: 'JS бекэнд фреймворк', correct: true),
+    Answer.new(body: 'Фронтэнд фреймворк')
+  ]},
+  { body: 'Что делает setState?', test: tests[2], answers: [
+    Answer.new(body: 'Обновляет состояние компонента', correct: true),
+    Answer.new(body: 'Возвращает текущее состояние компонента')
+  ]},
+  { body: 'Что такое ORM?', test: tests[0], answers: [
+    Answer.new(body: 'Object relation mapping', correct: true),
+    Answer.new(body: 'Object rational mapping')
+  ]},
+  { body: 'Для чего нужна библиотека Redux?', test: tests[2], answers: [
+    Answer.new(body: 'Для изменения dom-объектов'),
+    Answer.new(body: 'Для управления состоянием приложения', correct: true)
+  ]}
 ])
 
 user = User.create(email: 'TestUser', password_digest: 'TestPassword')
-UserTest.create([
+UserTest.create!([
   { user: user, test: tests[0] },
   { user: user, test: tests[1] },
   { user: user, test: tests[2] }
