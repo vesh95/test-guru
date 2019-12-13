50f6dc435ecac1dfc46af2b035e5e6a586b287c2
vesh95 <vesh95.17@ya.ru>
2019-12-13 09:32:33 +0500

    rollback names_by_category scope to method, add by_category scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -12,10 +12,10 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1)}
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-
   scope :level, -> (level) { where(level: level) }
+  scope :by_category, -> (name) { joins(:category).where(categories: { title: name }) }
 
-  scope :names_by_category, -> (name) do
-    joins(:category).where(categories: { title: name }).pluck(:title)
+  def self.names_by_category(name)
+    by_category(name).pluck(:title)
   end
 end
