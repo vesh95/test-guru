16e06286a6704f270c2ff2c3cd00d645706d741f
vesh95 <vesh95.17@ya.ru>
2019-12-14 12:45:01 +0500

    change size to count in answers_1_4_size_validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,6 +7,6 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   def answers_1_4_size_validation
-    errors.add(:question) unless question.answers.size <= 4
+    errors.add(:question) unless question.answers.count <= 4
   end
 end
