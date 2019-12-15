af66e9b655769092baa1aaebba0698501f3ace46
vesh95 <vesh95.17@ya.ru>
2019-12-15 18:55:09 +0500

    implements showing one question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,24 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @questions = Test.find(params[:test_id]).questions.all
     render json: @questions
+  end
+
+  def show
+    render json: { question: @question, answers: @question.answers }
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
   end
 end
