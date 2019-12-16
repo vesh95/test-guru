12557e78ddd3caf3c069fdf08e4a5b5e1879c8a4
vesh95 <vesh95.17@ya.ru>
2019-12-16 15:42:32 +0500

    implements standard CRUD actions tests controller

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,56 @@
+class TestsController < ApplicationController
+  before_action :set_test, only: %i[edit show update destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @test.destroy
+      redirect_to tests_path
+    else
+      redirect_to @test
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render file: 'public/404.html', status: :not_found
+  end
+end
