1d3e18e6f8a8c2e30bf07c255701fcdecc66d077
vesh95 <vesh95.17@ya.ru>
2019-12-16 22:32:03 +0500

    add current_year, github_url helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def question_header(test)
     "#{TITLES[action_name.to_sym]} #{test.title} question"
   end
+
+  def current_year
+    DateTime.now.strftime('%Y')
+  end
+
+  def github_ulr(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}"
+  end
 end
