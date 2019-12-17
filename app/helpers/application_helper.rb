df4de5c370c1eec859a54587571380cc2ec98f2f
vesh95 <vesh95.17@ya.ru>
2019-12-17 11:58:34 +0500

    move question_header helper to QuestionHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-  TITLES = {new: 'Create new', edit: 'Edit', show: nil}.freeze
-  def question_header(test)
-    "#{TITLES[action_name.to_sym]} #{test.title} question"
-  end
-
   def current_year
     DateTime.current.strftime('%Y')
   end
---
+++ app/helpers/question_helper.rb
@@ -0,0 +1,6 @@
+module QuestionHelper
+  TITLES = {new: 'Create new', edit: 'Edit', show: nil}.freeze
+  def question_header(test)
+    "#{TITLES[action_name.to_sym]} #{test.title} question"
+  end
+end
