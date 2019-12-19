fea04fcf26ead7353e0c540a9ba239e0131b5ff5
Eduard Larionov <vesh95.17@gmail.com>
2019-12-19 10:48:18 +0500

    Update app/models/user.rb
    Co-Authored-By: max-underthesun <max.underthesun@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true
 
   def test_passage(test)
-    test_passages.order(id: :desc).find_by(test_id: test.id)
+    test_passages.order(id: :desc).find_by(test: test)
   end
 
 
