3e0f70e444b29c0eb4e53c9104ef4f1e2b6e0648
vesh95 <vesh95.17@ya.ru>
2019-12-19 22:38:47 +0500

    add digest to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+  # has_secure_password
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id
@@ -9,8 +13,14 @@ class User < ApplicationRecord
     test_passages.order(id: :desc).find_by(test: test)
   end
 
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
 
-  # has_secure_password
+  def digest(strnig)
+    Digest::SHA1.hexdigest(string)
+  end
+
   def tests_by_level(level)
     tests.level(level)
   end
