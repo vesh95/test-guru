6febab429c4c6ee36bf7ada6e47f714b681c6a56
vesh95 <vesh95.17@ya.ru>
2019-12-19 09:36:52 +0500

    refactored inline actions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,15 +3,13 @@ class AnswersController < ApplicationController
   before_action :set_answer, only: %i[show edit update destroy]
   before_action :set_question, only: %i[new create]
 
-  def show
-  end
+  def show; end
 
   def new
     @answer = @question.answers.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @answer = @question.answers.new(answer_params)
