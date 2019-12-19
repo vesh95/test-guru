c9f14ad78851235301942d6e9550554427dd5059
vesh95 <vesh95.17@ya.ru>
2019-12-19 10:33:47 +0500

    join next question setter callbacks

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :set_next_question
+  before_validation :next_question
 
   def accept!(answer_ids)
     self. correct_questions += 1 if correct_answer?(answer_ids)
@@ -29,10 +28,6 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
   def correct_answer?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
@@ -42,10 +37,10 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
-  end
-
-  def set_next_question
-    self.current_question = next_question
+    self.current_question = if self.new_record?
+      test.questions.order(:id).first if test.present?
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 end
