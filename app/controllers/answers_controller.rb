148b052768eb7e6dfff6e4568c29fad78c8fcb98
vesh95 <vesh95.17@ya.ru>
2019-12-20 11:42:52 +0500

    add before authentication callback

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-
+  before_action :authenticate_user!
   before_action :set_answer, only: %i[show edit update destroy]
   before_action :set_question, only: %i[new create]
 
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_question, only: %i[edit show update destroy]
   before_action :set_test, only: %i[new create]
 
