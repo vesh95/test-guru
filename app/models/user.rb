55b9c5642f51e2b634209ee2e1830eafe52b6b93
vesh95 <vesh95.17@ya.ru>
2019-12-20 08:34:51 +0500

    change auth module to has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  include Auth
-
-  # has_secure_password
 
   validates :email, presence: true
   validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
@@ -12,6 +9,8 @@ class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
