d613ac94968ed3928c8ec7d5903297562a2c7006
vesh95 <vesh95.17@ya.ru>
2019-12-21 15:57:27 +0500

    moved validates after assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  validates :email, presence: true
-  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id
 
+  validates :email, presence: true
+  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  validates :password, confirmation: true
   validates :email, format: { with: /\w+@\w+[[.]\w+]*/i}
   validates :email, uniqueness: true
 
