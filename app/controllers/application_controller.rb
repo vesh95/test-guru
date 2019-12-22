67f4bfd6a4b39afa808b56d5854dca3c67a83966
vesh95 <vesh95.17@ya.ru>
2019-12-22 11:38:24 +0500

    add names fields to devise registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Привет, #{current_user.first_name || 'Аноним'}!"
     if current_user.is_a?(Admin)
       admin_tests_path
     else
       root_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
   end
 end
