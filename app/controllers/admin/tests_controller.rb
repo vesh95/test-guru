e661c3a762b1206856fb5f54c1f4200a1ebb086a
vesh95 <vesh95.17@ya.ru>
2019-12-22 18:34:58 +0500

    refactored set test author

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,8 +16,7 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def create
-    @test = Test.new(test_params)
-    @test.author = current_user
+    @test = current_user.own_tests.new(test_params)
     if @test.save
       redirect_to admin_test_path(@test)
     else
