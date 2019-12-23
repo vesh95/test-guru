82c8beee862be2101d896bd6f282dbc84c80962e
vesh95 <vesh95.17@ya.ru>
2019-12-23 11:22:45 +0500

    add default locale setting params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(user)
     if user.admin?
@@ -9,10 +10,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { lang: I18n.locale}
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  end
+
+  def set_locale
+    I18n.locale= I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ module TestGuru
   class Application < Rails::Application
     config.load_defaults 6.0
     config.time_zone = 'Ekaterinburg'
-    # config.i18n.default_locale = :ru
+    config.i18n.default_locale = :ru
   end
 end
