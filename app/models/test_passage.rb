0427686ef7e1aac5bf9b4dfde0e337e8746a76bf
vesh95 <vesh95.17@ya.ru>
2019-12-23 14:05:01 +0500

    fixed test_passage correct_answer? method

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -28,7 +28,8 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def correct_answer?(answer_ids = [])
+  def correct_answer?(answer_ids)
+    return if answer_ids.nil?
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
