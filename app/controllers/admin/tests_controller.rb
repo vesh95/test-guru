0b7513a549734a7edb79ecab5ec568a63c8dd3f1
vesh95 <vesh95.17@ya.ru>
2019-12-23 11:59:12 +0500

    add I18n to tests form

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -18,7 +18,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.own_tests.new(test_params)
     if @test.save
-      redirect_to admin_test_path(@test)
+      redirect_to admin_test_path(@test), notice: t('.success')
     else
       render :new
     end
