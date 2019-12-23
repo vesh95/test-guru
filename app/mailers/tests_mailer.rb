c94ec2c2fc5be4f75b6e51c7d92541400fcdfda2
vesh95 <vesh95.17@ya.ru>
2019-12-23 14:10:44 +0500

    add mail localization

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -3,6 +3,6 @@ class TestsMailer < ApplicationMailer
     @user = test_passage.user
     @test = test_passage.test
 
-    mail to: @user.email, subject: 'You completed the TestGuru test!'
+    mail to: @user.email
   end
 end
