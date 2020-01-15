b60fb9b3aeb9c305905fed79d9175da7f870d085
vesh95 <vesh95.17@ya.ru>
2020-01-16 00:25:45 +0500

    fixed subject in FeedbackMailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,6 +2,6 @@ class FeedbackMailer < ApplicationMailer
   def feedback(feedback_params)
     @text = feedback_params[:text]
     @author = feedback_params[:author]
-    mail to: Admin.first
+    mail to: Admin.first.email, subject: t('.subject')
   end
 end
