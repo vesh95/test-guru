53a84507fd29a177e5755f5215785b9709abf642
vesh95 <vesh95.17@ya.ru>
2020-01-21 18:26:52 +0500

    changed badges checks

--- app/models/badges_user.rb
+++ app/models/badges_user.rb
@@ -1,4 +1,7 @@
 class BadgesUser < ApplicationRecord
   belongs_to :user
   belongs_to :badge
+
+  scope :older_than_test, -> (test) { where("created_at > ?", test.created_at) }
+  scope :by_rules, -> (rule, rule_value) { joins(:badge).where(badges: { rule: rule, rule_value: rule_value }) }
 end
--- app/services/badges_service.rb
+++ app/services/badges_service.rb
@@ -20,18 +20,25 @@ class BadgesService
   private
 
   def first_try?
-    @test_passage.success? && @user.tests.where(id: @test_passage.test.id).count == 1
+    return unless @test_passage.success?
+    @user.tests.where(id: @test_passage.test.id, test_passages: { success: true}).count == 1
   end
 
   def all_for_category?(rule_value)
-    return if @test_passage.test.category == rule_value
-    @user.success_tests.by_category(rule_value)
-         .pluck(:id).sort == Test.by_category(rule_value).pluck(:id)
+    return if @test_passage.test.category != rule_value
+
+    last_test = Test.by_category(rule_value).last
+    return unless @user.badges_users.older_than_test(last_test).by_rules(__method__, rule_value).empty?
+
+    (Test.by_category(rule_value).pluck(:id) - @user.success_tests.by_category(rule_value).pluck(:id)).empty?
   end
 
   def all_level_of?(rule_value)
-    return if @test_passage.test.level == rule_value
-    @test_passage.test.level == rule_value && @user.success_tests.level(rule_value)
-         .pluck(:id).sort == Test.level(rule_value).pluck(:id).sort
+    return if @test_passage.test.level != rule_value.to_i
+
+    last_test = Test.all.level(rule_value).last
+    return unless @user.badges_users.older_than_test(last_test).by_rules(__method__, rule_value).empty?
+
+    (Test.level(rule_value).pluck(:id) - @user.success_tests.level(rule_value).pluck(:id)).empty?
   end
 end
