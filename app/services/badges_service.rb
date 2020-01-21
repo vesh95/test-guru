7d40cd4552031a40218fa5eb5616ae83854e5e5c
vesh95 <vesh95.17@ya.ru>
2020-01-21 10:03:24 +0500

    add before db query checks

--- app/services/badges_service.rb
+++ app/services/badges_service.rb
@@ -24,11 +24,13 @@ class BadgesService
   end
 
   def all_for_category?(rule_value)
+    return if @test_passage.test.category == rule_value
     @user.success_tests.by_category(rule_value)
          .pluck(:id).sort == Test.by_category(rule_value).pluck(:id)
   end
 
   def all_level_of?(rule_value)
+    return if @test_passage.test.level == rule_value
     @test_passage.test.level == 1 && @user.success_tests.level(rule_value)
          .pluck(:id).sort == Test.level(rule_value).pluck(:id).sort
   end
